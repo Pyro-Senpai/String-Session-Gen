55493306cf8523b4744290d7ae6e6d8428e40064
Wizard <codenexusbotz@gmail.com>
2026-08-13 00:16:20 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -3,6 +3,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 import config
+import db
 
 def get_start_keyboard():
     return InlineKeyboardMarkup([
@@ -14,6 +15,29 @@ def get_back_close_keyboard():
     return InlineKeyboardMarkup([
         [InlineKeyboardButton("Back", callback_data="back"), InlineKeyboardButton("Close", callback_data="close")]
     ])
+
+@Client.on_message(filters.command("start") & filters.private)
+async def start_handler(client: Client, message: Message):
+    user = message.from_user
+    await db.add_user(client, message)
+    mention = user.mention if user else "User"
+
+    effect_id = int(config.MESSAGE_EFFECT_ID) if hasattr(config, 'MESSAGE_EFFECT_ID') and config.MESSAGE_EFFECT_ID else None
+
+    if config.START_PIC:
+        await message.reply_photo(
+            photo=config.START_PIC,
+            caption=config.START_MSG.format(mention=mention),
+            reply_markup=get_start_keyboard(),
+            message_effect_id=effect_id
+        )
+    else:
+        await message.reply_text(
+            text=config.START_MSG.format(mention=mention),
+            reply_markup=get_start_keyboard(),
+            message_effect_id=effect_id,
+            disable_web_page_preview=True
+        )
 
 @Client.on_callback_query(filters.regex("about"))
 async def about_callback(client: Client, callback_query):
@@ -32,10 +56,9 @@ async def help_callback(client: Client, callback_query):
 @Client.on_callback_query(filters.regex("back"))
 async def back_callback(client: Client, callback_query):
     user = callback_query.from_user
-    mention = user.mention
+    mention = user.mention if user else "User"
     effect_id = int(config.MESSAGE_EFFECT_ID) if hasattr(config, 'MESSAGE_EFFECT_ID') and config.MESSAGE_EFFECT_ID else None
     
-    # பழைய மெசேஜை டெலிட் செய்துவிட்டு அதே ஐடியில் புதிய போட்டோ அனுப்ப விரும்பினால்:
     await callback_query.message.delete()
     await client.send_photo(
         chat_id=callback_query.message.chat.id,
