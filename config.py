bed44891e756bd793f77f08cfaf54aa850f543c8
Wizard <codenexusbotz@gmail.com>
2026-08-13 02:06:41 +0530

    Update START_MSG with new formatting and link

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ API_ID = int(os.environ.get("API_ID", "123456"))
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-START_MSG = "<b>💖 ʜᴇʏ {mention}! 🥀,</b>\n<b><blockquote>ɪ ᴄᴀɴ ʜᴇʟᴘ ʏᴏᴜ ɢᴇɴᴇʀᴀᴛᴇ sᴇᴄᴜʀᴇ ᴘʏʀᴏɢʀᴀᴍ ᴀɴᴅ ᴘʏʀᴏғᴏʀᴋ sᴛʀɪɴɢ sᴇssɪᴏɴs ғᴏʀ ʏᴏᴜʀ ᴛᴇʟᴇɢʀᴀᴍ ᴀᴄᴄᴏᴜɴᴛs sᴀғᴇʟʏ ᴀɴᴅ ᴇᴀsɪʟʏ.</blockquote></b>\n<b><blockquote>ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ : <a href=" https://t.me/PyroSznpai">Pyro Senpai</a></blockquote></b>"
+START_MSG = "<b>💖 ʜᴇʏ {mention}! 🥀,</b>\n<b><blockquote>ɪ ᴄᴀɴ ʜᴇʟᴘ ʏᴏᴜ ɢᴇɴᴇʀᴀᴛᴇ sᴇᴄᴜʀᴇ ᴘʏʀᴏɢʀᴀᴍ ᴀɴᴅ ᴘʏʀᴏғᴏʀᴋ sᴛʀɪɴɢ sᴇssɪᴏɴs ғᴏʀ ʏᴏᴜʀ ᴛᴇʟᴇɢʀᴀᴍ ᴀᴄᴄᴏᴜɴᴛs sᴀғᴇʟʏ ᴀɴᴅ ᴇᴀsɪʟʏ.</blockquote></b>\n<b><blockquote>ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ: <a href="https://t.me/PyroSznpai">Pyro Senpai</a></blockquote></b>"
 START_PIC = "https://graph.org/file/e08200bae7bae43e9c89d-0c7c4b63a1de688a36.jpg"
 
 ABOUT_TEXT = "<b><blockquote>🌀 ᴛʜɪs ɪs ᴀ sɪᴍᴘʟᴇ ᴘʏʀᴏɢʀᴀᴍ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙoᴛ.</blockquote></b>"
