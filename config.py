027a9528d409265bfa90d950d3de7f4d942f5d01
Wizard <codenexusbotz@gmail.com>
2026-08-13 01:56:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,10 +9,10 @@ API_ID = int(os.environ.get("API_ID", "123456"))
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-START_MSG = "<b>💖 ʜᴇʏ {mention}! 🥀,</b>\n<b><blockquote>ɪ ᴄᴀɴ ʜᴇʟᴘ ʏᴏᴜ ɢᴇɴᴇʀᴀᴛᴇ sᴇᴄᴜʀᴇ ᴘʏʀᴏɢʀᴀᴍ ᴀɴᴅ ᴘʏʀᴏғᴏʀᴋ sᴛʀɪɴɢ sᴇssɪᴏɴs ғᴏʀ ʏᴏᴜʀ ᴛᴇʟᴇɢʀᴀᴍ ᴀᴄᴄᴏᴜɴᴛs sᴀғᴇʟʏ ᴀɴᴅ ᴇᴀsɪʟʏ.</blockquote></b>"
-START_PIC = "https://telegra.ph/file/aad055c98c566adfb7dcd-b42f72ff4d1de29e86.jpg"
+START_MSG = "<b>💖 ʜᴇʏ {mention}! 🥀,</b>\n<b><blockquote>ɪ ᴄᴀɴ ʜᴇʟᴘ ʏᴏᴜ ɢᴇɴᴇʀᴀᴛᴇ sᴇᴄᴜʀᴇ ᴘʏʀᴏɢʀᴀᴍ ᴀɴᴅ ᴘʏʀᴏғᴏʀᴋ sᴛʀɪɴɢ sᴇssɪᴏɴs ғᴏʀ ʏᴏᴜʀ ᴛᴇʟᴇɢʀᴀᴍ ᴀᴄᴄᴏᴜɴᴛs sᴀғᴇʟʏ ᴀɴᴅ ᴇᴀsɪʟʏ.</blockquote></b>\n<b><blockquote>ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ : <a href=" https://t.me/PyroSznpai">Pyro Senpai</a></blockquote></b>"
+START_PIC = "https://graph.org/file/e08200bae7bae43e9c89d-0c7c4b63a1de688a36.jpg"
 
-ABOUT_TEXT = "<b><blockquote>🌀 ᴛʜɪs ɪs ᴀ sɪᴍᴘʟᴇ ᴘʏʀᴏɢʀᴀᴍ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ boᴛ.</blockquote></b>"
+ABOUT_TEXT = "<b><blockquote>🌀 ᴛʜɪs ɪs ᴀ sɪᴍᴘʟᴇ ᴘʏʀᴏɢʀᴀᴍ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙoᴛ.</blockquote></b>"
 HELP_TEXT = "<b><blockquote>❓ ʜᴇʀᴇ ᴀʀᴇ ᴛʜᴇ ᴀᴠᴀɪʟᴀʙʟᴇ ᴄᴏᴍᴍᴀɴᴅs:\n/start - sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ\n/generate - ɢᴇɴᴇʀᴀᴛᴇ ᴀ sᴛʀɪɴɢ sᴇssɪᴏɴ</blockquote></b>"
 
 MESSAGE_EFFECT_ID = "5104841245755180586"
