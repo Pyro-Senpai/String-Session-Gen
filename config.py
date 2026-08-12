db14842fb0000a1d558b61cde8fea09446ccee28
Wizard <codenexusbotz@gmail.com>
2026-08-12 23:19:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,11 +9,11 @@ API_ID = int(os.environ.get("API_ID", "123456"))
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-START_MSG = "👋 Hello! Send /generate to create a Pyrofog String Session."
+START_MSG = "<b>💖 ʜᴇʏ {mention}! 🥀,</b>\n<b><blockquote>ɪ ᴄᴀɴ ʜᴇʟᴘ ʏᴏᴜ ɢᴇɴᴇʀᴀᴛᴇ sᴇᴄᴜʀᴇ ᴘʏʀᴏɢʀᴀᴍ ᴀɴᴅ ᴘʏʀᴏғᴏʀᴋ sᴛʀɪɴɢ sᴇssɪᴏɴs ғᴏʀ ʏᴏᴜʀ ᴛᴇʟᴇɢʀᴀᴍ ᴀᴄᴄᴏᴜɴᴛs sᴀғᴇʟʏ ᴀɴᴅ ᴇᴀsɪʟʏ.</blockquote></b>"
 START_PIC = "https://telegra.ph/file/aad055c98c566adfb7dcd-b42f72ff4d1de29e86.jpg"
 
-ABOUT_TEXT = "ℹ️ This is a simple session generator bot built with Pyrofog and Pyromode."
-HELP_TEXT = "❓ Here are the available commands:\n/start - Start the bot\n/generate - Generate a session"
+ABOUT_TEXT = "<b><blockquote>🌀 ᴛʜɪs ɪs ᴀ sɪᴍᴘʟᴇ ᴘʏʀᴏɢʀᴀᴍ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ boᴛ.</blockquote></b>"
+HELP_TEXT = "❓ ʜᴇʀᴇ ᴀʀᴇ ᴛʜᴇ ᴀᴠᴀɪʟᴀʙʟᴇ ᴄᴏᴍᴍᴀɴᴅs:\n/start - sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ\n/string - ɢᴇɴᴇʀᴀᴛᴇ ᴀ sᴛʀɪɴɢ sᴇssɪᴏɴ"
 
 MESSAGE_EFFECT_ID = "5104841245755180586"
 PORT = int(os.environ.get("PORT", "8080"))
