f73d27c0a7251cee3025d0f18f365eaafa8b640a
Wizard <codenexusbotz@gmail.com>
2026-08-12 22:52:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,15 @@
-# (©)Pyro-Senpai
-
 from aiohttp import web
 from pyrofork import Client
 import config
 import asyncio
 
-# Aiohttp web server for Koyeb health check
 async def handle(request):
     return web.Response(text="Bot is running!")
 
 app_web = web.Application()
 app_web.add_routes([web.get("/", handle)])
 
-async class Application:
+class Application:
     def __init__(self):
         self.bot = Client(
             "SessionGeneratorBot",
@@ -26,7 +23,6 @@ async class Application:
         await self.bot.start()
         print("Bot is Started.")
         
-        # Start aiohttp web server
         runner = web.AppRunner(app_web)
         await runner.setup()
         site = web.TCPSite(runner, "0.0.0.0", config.PORT)
