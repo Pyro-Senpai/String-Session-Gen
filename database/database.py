13165e7199f0c5992f688eb0222b70d49c86bc7f
Wizard <codenexusbotz@gmail.com>
2026-08-13 00:49:53 +0530

    Add 'mention' field to user dictionary

--- database/database.py
+++ database/database.py
@@ -21,7 +21,8 @@ async def add_user(client, message):
             '_id': user.id,
             'username': user.username,
             'first_name': user.first_name,
-            'last_name': user.last_name
+            'last_name': user.last_name, 
+            'mention': user.mention
         }
         try:
             await user_data.insert_one(user_dict)
